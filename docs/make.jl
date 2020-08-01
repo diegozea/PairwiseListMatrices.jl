2cc33120760249ea10b2b688d12e4d596965f7df
Elin- <elinteppa@gmail.com>
2020-08-01 18:52:26 +0200

    Update Travis and Docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,21 @@
 using Documenter, PairwiseListMatrices
 
 makedocs(
-    doctest = true,
-    format = :html,
-    sitename = "PairwiseListMatrices",
-    modules = [PairwiseListMatrices],
-    pages = [
+    doctest=true,
+    format=Documenter.HTML(
+        prettyurls=get(ENV, "CI", nothing) == "true"
+    ),
+    sitename="PairwiseListMatrices",
+    modules=[PairwiseListMatrices],
+    pages=[
         "index.md",
         "api.md"
     ]
 )
 
 deploydocs(
-    repo   = "github.com/diegozea/PairwiseListMatrices.jl.git",
-    target = "build",
-    deps   = nothing,
-    make   = nothing
+    repo="github.com/diegozea/PairwiseListMatrices.jl.git",
+    target="build",
+    deps=nothing,
+    make=nothing
 )
